cafba1d477318679d5df84d553667ab4d4461854
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-02-23 18:06:57 +0100

    wave done

--- main.js
+++ main.js
@@ -18,16 +18,45 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load("/textures/particles/2.png");
 
 /**
- * Test cube
+ * Particles
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-);
-scene.add(cube);
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 20000;
 
+const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+
+for (let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10;
+    colors[i] = Math.random();
+}
+
+particlesGeometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positions, 3)
+);
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    sizeAttenuation: true,
+    color: "#ff88cc",
+    alphaMap: particleTexture,
+    // alphaTest: 0.1,
+    // depthTest: false,
+    depthWrite: false,
+    transparent: true,
+    vertexColors: true,
+});
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 /**
  * Sizes
  */
@@ -60,7 +89,7 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     100
 );
-camera.position.z = 3;
+camera.position.z = 10;
 scene.add(camera);
 
 // Controls
@@ -83,6 +112,18 @@ const clock = new THREE.Clock();
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime();
+
+    // Update particles
+    // particles.rotation.y = elapsedTime * 0.2;
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3;
+        const x = particlesGeometry.attributes.position.array[i3 + 0];
+
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(
+            elapsedTime + x
+        );
+    }
+    particlesGeometry.attributes.position.needsUpdate = true;
 
     // Update controls
     controls.update();
